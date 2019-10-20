8d6c322b60b382e377121325ff04dcd2c8f52746
Jurgis Verbys <31169477+ewn5@users.noreply.github.com>
2019-10-20 23:46:29 +0300

    Tidy up routes.

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,11 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::namespace('Admin')->group(function () {
-	Route::middleware('admin')->group(function () {
-		Route::prefix('admin')->group(function () {
-			Route::name('admin.')->group(function () {
-				Route::resource('articles', 'ArticleController');
-			});
-		});
-	});
+Route::group([
+	'namespace' => 'Admin',
+	'middleware' => 'admin',
+	'prefix' => 'admin',
+	'as' => 'admin.',
+	], function () {
+		Route::resource('articles', 'ArticleController');
 });
